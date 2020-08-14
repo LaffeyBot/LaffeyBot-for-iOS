2d4fdbece072fdfe1ec574c263f76f85117e6397
zjdavid <zjdavid.2003@gmail.com>
2020-08-14 14:35:15 +0800

    Initial Commit

---
+++ Laffey/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Laffey
+//
+//  Created by 戴元平 on 8/14/20.
+//  Copyright © 2020 Wei Dai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

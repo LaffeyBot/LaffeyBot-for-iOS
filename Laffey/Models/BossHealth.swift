e0c3bcbab87bb358cb316d2655cba049c09d8324
zjdavid <zjdavid.2003@gmail.com>
2020-08-16 19:06:58 +0800

    Add boss health

---
+++ Laffey/Models/BossHealth.swift
@@ -0,0 +1,9 @@
+//
+//  BossHealth.swift
+//  Laffey
+//
+//  Created by 戴元平 on 8/16/20.
+//  Copyright © 2020 Wei Dai. All rights reserved.
+//
+
+let bossMaxHealth = [6000000, 8000000, 10000000, 12000000, 20000000]
